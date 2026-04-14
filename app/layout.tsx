09fc32612c9e9f37d49a49d0c53b358568f605c4
jangho0806-hub <jangho0806@gmail.com>
2026-04-14 18:45:51 +0900

    Update metadata description for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "GSM자동화세차기",
-  description: "자동차 자동화 세차기 제조 · 판매 전문기업",
+  description: "터널세차기,문형세차기(소형대형),노터치세차기,코인셀프세차장 제조시공업체",
 verification: {
     other: {
       "naver-site-verification": "add1cd5cd597ce68e8c1ead9faf98899593045b5",
