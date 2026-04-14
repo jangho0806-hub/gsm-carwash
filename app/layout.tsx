4a3ef2d0ea3e4280d7b52c43ad5be2313df862d8
jangho0806-hub <jangho0806@gmail.com>
2026-04-14 11:09:16 +0900

    Add Naver site verification to metadata
    Added site verification metadata for Naver.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,12 @@ import "./globals.css";
 export const metadata = {
   title: "GSM자동화세차기",
   description: "자동차 자동화 세차기 제조 · 판매 전문기업",
+verification: {
+    other: {
+      "naver-site-verification": "add1cd5cd597ce68e8c1ead9faf98899593045b5",
+    },
+  },
 };
-
 export default function RootLayout({
   children,
 }: {
